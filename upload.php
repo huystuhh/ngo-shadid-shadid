b6a2ec5d393890a3f59017cf03304bce036a9350
hngo4619 <hngo4619@outlook.com>
2015-04-06 08:09:58 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
 // of $_FILES.
 
-$uploaddir = '/var/www/ngo-shadid-shadid/Database/';
+$uploaddir = "/var/www/ngo-shadid-shadid/Database/";
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
 echo '<pre>';
