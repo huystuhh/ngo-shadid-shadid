947bc64f8f86a6a4abd198c9c10a6917af3bfa14
Sam <Samuel.A.Shadid>
2015-04-07 03:28:11 -0500

    adsf

--- OculusConnection/ConnectionTest/js/connection.js
+++ OculusConnection/ConnectionTest/js/connection.js
@@ -20,7 +20,7 @@ function initScene() {
 	windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
 	aspectRatio = window.innerWidth / window.innerHeight;
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 10000);
+	camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 2000);
 	camera.position.z = 100;
 	camera.lookAt(scene.position);
 	// Initialize the renderer
@@ -33,7 +33,7 @@ function initScene() {
 
 function initLights(){
 	point = new THREE.DirectionalLight( 0xffeedd );
-	point.position.set( 0, 0, 1 );
+	point.directionalLight.position.set( 0, 0, 1 );
 	scene.add(point);
 	
 	var ambient = new THREE.AmbientLight( 0x101030 );
