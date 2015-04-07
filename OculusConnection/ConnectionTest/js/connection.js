71c1b71c9499201845e2074be15080546696a235
Sam <Samuel.A.Shadid>
2015-04-07 03:26:55 -0500

    dsaf

--- OculusConnection/ConnectionTest/js/connection.js
+++ OculusConnection/ConnectionTest/js/connection.js
@@ -33,7 +33,7 @@ function initScene() {
 
 function initLights(){
 	point = new THREE.DirectionalLight( 0xffeedd );
-	point.directionalLight.position.set( 0, 0, 1 );
+	point.position.set( 0, 0, 1 );
 	scene.add(point);
 	
 	var ambient = new THREE.AmbientLight( 0x101030 );
