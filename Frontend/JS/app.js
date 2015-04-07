f26b5510cbd5a8743f5f5fa6954fed5161998757
Huystuhh <Huystuhh@Huy-SP3>
2015-04-06 23:18:58 -0500

    bug fix

--- Frontend/JS/app.js
+++ Frontend/JS/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 			url: "upload.php",	// Upload.php file stored on the web server, don't change
 			paramName: "userfile", // must be named this, don't change
 			maxFiles: 1, // Only want the user to riftify one thing at a time
-			maxFileSize: 2048 //MB = 2 GB
+			maxFileSize: 2048, //MB = 2 GB
 			init: function() {
 				this.on("success", function(file) {
 					document.getElementById("riftify").visibility = 'visible';
