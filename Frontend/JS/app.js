03accf7504175bf9188c960d93b7a195b8b97c51
Huystuhh <Huystuhh@Huy-SP3>
2015-04-06 23:17:43 -0500

    Stuff

--- Frontend/JS/app.js
+++ Frontend/JS/app.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 	Dropzone.options.myAwesomeDropzone = { // The camelized version of the ID of the form element
-			url: "upload.php",	// Upload.php file stored on the web server
-			paramName: "userfile", // must be named this
+			url: "upload.php",	// Upload.php file stored on the web server, don't change
+			paramName: "userfile", // must be named this, don't change
 			maxFiles: 1, // Only want the user to riftify one thing at a time
 			maxFileSize: 2048 //MB = 2 GB
+			init: function() {
+				this.on("success", function(file) {
+					document.getElementById("riftify").visibility = 'visible';
+				}
+			}
 }});
