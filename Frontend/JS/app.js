bfa8bb172b79686b665f243b52abd580574f7163
Huystuhh <Huystuhh@Huy-SP3>
2015-04-15 00:42:39 -0500

    Revert after Testing Stuff

--- Frontend/JS/app.js
+++ Frontend/JS/app.js
@@ -2,18 +2,16 @@ var renderer, camera, scene, element;
 var point;
 var aspectRatio, windowHalf;
 var object;
-var accelerationIndicator;
-var oculusBridge;
-var usingRift = false;
 var riftCamera;
-var goLeft = false
-var goRight = false
-var goUp = false
-var goDown = false
-var rotationx = rotationy = rotationz = 0    
+var usingRift = false;
+var goLeft = false;
+var goRight = false;
+var goUp = false;
+var goDown = false;
+var rotationx = rotationy = rotationz = 0;   
+var _q1 = new THREE.Quaternion(); 
 var axisX = new THREE.Vector3( 1, 0, 0 ); 
-var axisZ = new THREE.Vector3( 0, 0, 1 );
-var uploadedFile;
+var axisZ = new THREE.Vector3( 0, 0, 1 ); 
 
 $(document).ready(function(){
 	Dropzone.options.myAwesomeDropzone = { // The camelized version of the ID of the form element
@@ -25,123 +23,37 @@ $(document).ready(function(){
 				this.on("success", function(file) {
 					document.getElementById('riftify').style.display = "inline-block";
 					localStorage.setItem("obj", file);
-					uploadedFile = file;
 					console.log(file);
 				});
 			}
 }});
 
-$(function(){
-	$("#riftify, #modal-background, #modal-close").click(function () {
-		$("#modal-content,#modal-background").toggleClass("active");
-		riftify();
-	});
-});
-
 function riftify() {
-	init();
-	animate();
-	//window.open("../../OculusConnection/ConnectionTest/connection.html");
+	window.open("../../OculusConnection/ConnectionTest/connection.html");
 }
 
-function initScene() {
-	clock = new THREE.Clock();
-	mouse = new THREE.Vector2(0, 0);
-	windowHalf = new THREE.Vector2( $("#modal-content").width() / 2, $("#modal-content").height() / 2);
-	aspectRatio = $("#modal-content").width() / $("#modal-content").height();
-	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 2000);
-	camera.position.z = 25;
-	camera.lookAt(scene.position);
-	// Initialize the renderer
-	renderer = new THREE.WebGLRenderer({antialias:true});
-	renderer.setClearColor(0x161616);
-	renderer.setSize($("#modal-content").width(), $("#modal-content").height());
-	element = document.getElementById('modal-content');
-	element.appendChild(renderer.domElement);
-}
-
-function initLights(){
-	point = new THREE.DirectionalLight( 0xffeedd );
-	point.position.set( 0, 0, 1 );
-	scene.add(point);
-	
-	var ambient = new THREE.AmbientLight( 0x101030 );
-	scene.add( ambient );
-
-
-}
-function initGeometry(){
-	//methods for OBJLoader
-	var onProgress = function ( xhr ) 
+function onResize() 
+{
+	if(!usingRift)
 	{
-		if ( xhr.lengthComputable ) 
-		{
-			var percentComplete = xhr.loaded / xhr.total * 100;
-			console.log( Math.round(percentComplete, 2) + '% downloaded' );
-		}
-	};
-
-	var onError = function ( xhr ) 
-	{
-	};
-	
-	var manager = new THREE.LoadingManager();
-	var texture = new THREE.Texture(manager);
-	var loader = new THREE.ImageLoader( manager );
-	var material = new THREE.MeshLambertMaterial({ color: 0x29d6e1, emissive:0x297d67});
-	
-	var loader = new THREE.OBJLoader(manager);
-	console.log(uploadedFile);
-	loader.load(uploadedFile, function(obj)
-			{
-				obj.traverse( function ( child ) {
-					if ( child instanceof THREE.Mesh ) {
-						child.material.map = texture;
-					}
-				} );
-				object.add(obj);
-				scene.add(object);
-			}, onProgress, onError);
-	
-}
-
-function onResize() {
-	if(!usingRift){
 		windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
 		aspectRatio = window.innerWidth / window.innerHeight;
 		camera.aspect = aspectRatio;
 		camera.updateProjectionMatrix();
 		renderer.setSize(window.innerWidth, window.innerHeight);
-	}else {
+	}//end of if
+	else 
+	{
 		riftCamera.setSize(window.innerWidth, window.innerHeight);
-	}
-	
-}
-function bridgeConfigUpdated(config){
-	console.log("Oculus config updated.");
-	riftCamera.setHMD(config);
-}
-function bridgeAccelerationUpdated(accel) {
-	// scale values so 1g = 20 world units
-	accelerationIndicator.children[0].position.x = (accel.x * 1.02040816326531) * 2;
-	accelerationIndicator.children[1].position.x = (accel.y * 1.02040816326531) * 2;
-	accelerationIndicator.children[2].position.x = (accel.z * 1.02040816326531) * 2;
-}
-function bridgeOrientationUpdated(quat) {
-	referenceCube.quaternion.set(quat.x, quat.y, quat.z, quat.w);
-}
-function bridgeConnected(){
-	document.getElementById("logo").className = "";
-}
-function bridgeDisconnected(){
-	document.getElementById("logo").className = "offline";
-}
-function animate() 
+	}//end of else
+}//end of onResize
+
+function animate()
 {
 	requestAnimationFrame(animate);
 	render();
-}
+}//end of animate
+
 function render() {
 	try{
 		if(usingRift)
@@ -181,6 +93,20 @@ function render() {
 }
 function init()
 {
+	//methods for OBJLoader
+	var onProgress = function ( xhr ) 
+	{
+		if ( xhr.lengthComputable ) 
+		{
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+
+	var onError = function ( xhr ) 
+	{
+	};
+
 	//initialize the object.
 	object = new THREE.Object3D();
 	var axes = new THREE.AxisHelper();
@@ -189,13 +115,50 @@ function init()
 	document.addEventListener("keydown", keyDown, false);
 	document.addEventListener("keyup", keyUp, false);
 	window.addEventListener('resize', onResize, false);
-	initScene();
-	initGeometry();
-	initLights();
+	
 	//button for rift camera
+	//document.getElementById("toggle-render").addEventListener("click", function(){
+	//	usingRift = !usingRift;
+	//	onResize();
+	//});
+		
+	//initialze the scene
+	windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+	aspectRatio = window.innerWidth / window.innerHeight;
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 10000);
+	camera.position.set(120, 160, 120);
+	camera.lookAt(scene.position);
+	
+	// Initialize the renderer
+	renderer = new THREE.WebGLRenderer({antialias:true});
+	renderer.setClearColor(0x161616);
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	element = document.getElementById('viewport');
+	element.appendChild(renderer.domElement);
+	
+	//Let there be Light.
+	point = new THREE.DirectionalLight( 0xffffff, 1, 0, Math.PI, 1 );
+	point.position.set( -250, 250, 150 );
+	scene.add(point);
+	
+	//create object
+	var material = new THREE.MeshLambertMaterial({ color: 0x29d6e1, emissive:0x297d67});
+	var manager = new THREE.LoadingManager();
+	var loader = new THREE.OBJLoader(manager);
+	loader.load(localStorage.getItem("obj"), function(obj)
+			{
+				object.add(obj)
+				scene.add(object);
+				
+			}, onProgress, onError);
+	
 	
 	riftCamera = new THREE.OculusRiftEffect(renderer);
-}
+	usingRift = !usingRift;
+	onResize();
+}//end of init
 
 function keyDown(event)
 {
@@ -219,7 +182,7 @@ function keyDown(event)
 		goDown = true;
 		goUp = false;
 	}//end if
-}
+}//end of keyDown
 
 function keyUp()
 {
